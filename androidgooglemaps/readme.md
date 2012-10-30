be240dcbdaf4ee27fd36108211c9101ddbe94487
Burr Sutter <burrsutter@gmail.com>
2012-10-30 17:30:17 -0400

    adding googlemaps

---
+++ androidgooglemaps/readme.md
@@ -0,0 +1,4 @@
+Demonstrates the use of Cordova/Phonegap's ability to grab the users current
+Lat/Long and then draw it on a Google Map.
+
+Nothing fancy, no pinch to zoom or scrolling - just put a marker on a map
---
+++ androidgooglemaps/src/com/burrsutter/android/googlemaps/MainActivity.java
@@ -0,0 +1,23 @@
+package com.burrsutter.android.googlemaps;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
