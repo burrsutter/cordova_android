65f2374236cc9822880fd2573e0a3304ac7436c5
Burr Sutter <burrsutter@gmail.com>
2012-10-30 17:02:14 -0400

    initial checkin

---
+++ androidcordovadefault/gen/com/burrsutter/android/cordovadefault/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.burrsutter.android.cordovadefault;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
